eb5aa22a034ffe526e5d429032e91d9a21d085ff
LovelyAndy <andrewkarwoski9@gmail.com>
2021-03-18 08:33:12 -0500

    hero title positioning fixed

--- andykarwoski.com/src/components/sections/Hero.vue
+++ andykarwoski.com/src/components/sections/Hero.vue
@@ -2,9 +2,9 @@
   <div class="hero">
     <ProfilePicPF />
     <div class="title-els">
-      <div class="title-els_name">
-        <div class="t-main-title andy">Andy</div>
-        <div class="t-main-title karwoski">Karwoski</div>
+      <div class="title-els_main">
+        <span class="t-main-title andy">Andy</span>
+        <span class="t-main-title karwoski">Karwoski</span>
       </div>
       <div class="title-els_sub">
         <span class="t-section-title-md frontend">frontend web developer</span>
@@ -40,16 +40,26 @@ export default {
 .title-els
   display: flex
   flex-direction: column
-.title-els_name
+  margin-left: -2rem
+.title-els_main
   text-align: left
   z-index: 3
-
-.andy,
-.designer
-  color: $c-red
-.karwoski,
-.frontend
-  color: $c-blue
+  .andy
+    display: block
+    color: $c-red
+  .karwoski
+    display: block
+    margin-top: -3.78rem
+    margin-left: 3.3rem
+    color: $c-blue
+.title-els_sub
+  text-align: center
+  margin-top: -1.5rem
+  margin-left: 3.5rem
+  .designer
+    color: $c-red
+  .frontend
+    color: $c-blue
 
 .line
   color: $c-gold
